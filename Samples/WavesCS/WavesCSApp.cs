79c7e05d77839347bc0283a51a8a4a6b234242ff
Discosultan <j2v@neti.ee>
2016-07-24 16:40:26 +0300

    Add all compute and tessellation sample skeletons

--- Samples/WavesCS/WavesCSApp.cs
+++ Samples/WavesCS/WavesCSApp.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
-using System.Runtime.InteropServices;
 using System.Threading;
 using SharpDX;
 using SharpDX.Direct3D;
