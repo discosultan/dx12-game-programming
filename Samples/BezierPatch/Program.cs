71e3958e595ce9b3bf0836d246a484009922f78b
Discosultan <j2v@neti.ee>
2016-07-25 09:53:25 +0300

    Fix broken sln build

---
+++ Samples/BezierPatch/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Diagnostics;
+
+namespace DX12GameProgramming
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //using (D3DApp app = new BezierPatchApp(Process.GetCurrentProcess().Handle))
+            //{
+            //    app.Initialize();
+            //    app.Run();
+            //}
+        }
+    }
+}
